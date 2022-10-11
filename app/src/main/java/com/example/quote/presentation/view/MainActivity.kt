37dce75b848609cc6cb778488094740568a74bb0
asyncr <carrascoruizjesus@gmail.com>
2022-10-10 19:25:54 -0500

    Add Pattern Observer

--- app/src/main/java/com/example/quote/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/quote/presentation/view/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.quote.presentation.view
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.quote.databinding.ActivityMainBinding
 import com.example.quote.presentation.viewmodel.QuoteViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private val quoteViewModel: QuoteViewModel by viewModels()
 
@@ -18,6 +18,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+        observer()
+        quoteViewModel.randomQuote()
+        binding.viewContainer.setOnClickListener{
+            quoteViewModel.randomQuote()
+        }
     }
 
+    private fun observer() {
+        lifecycleScope.launch {
+            quoteViewModel.quoteModel.collect {
+                binding.tvQuote.text = it.quote
+                binding.tvAuthor.text = it.author
+            }
+        }
+    }
 }
